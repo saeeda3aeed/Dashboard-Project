26dbfa97d68f02fd57c3bd5e8f802ec73a059813
Ahmed Hamza <ahmedhassanhamza.cs@gmail.com>
2023-04-16 13:11:04 +0200

    before adding table components

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { InfoComponent } from './components/info/info.component';
+import { UserComponent } from './components/user/user.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { SidenavWrapperComponent } from './components/sidenav-wrapper/sidenav-wrapper.component';
+import { MatMenuModule } from '@angular/material/menu';
+
+
+@NgModule({
+  declarations: [SidenavWrapperComponent, DashboardComponent, InfoComponent, UserComponent],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    // NG Material Modules
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,MatMenuModule
+  ]
+})
+export class DashboardModule { }
